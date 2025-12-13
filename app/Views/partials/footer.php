58a2e5939d2558db2dffcbe3fcba32827ca9bc2b
Cường <tranthanhcuongit@gmail.com>
2025-12-13 10:08:01 +0700

    oke

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -7,27 +7,27 @@
                 <img src="./assest/img/logo_qnu_footer.png" alt="Quy Nhon University Logo">
             </div>
             <div class="social-icons">
-                <a href="#" class="social-icon" aria-label="Facebook">
+                <a href="https://www.facebook.com/daihocquynhon" class="social-icon" aria-label="Facebook">
                     <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
                     </svg>
                 </a>
-                <a href="#" class="social-icon" aria-label="TikTok">
+                <a href="https://www.tiktok.com/@daihocquynhon" class="social-icon" aria-label="TikTok">
                     <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/>
                     </svg>
                 </a>
-                <a href="#" class="social-icon" aria-label="Messenger">
+                <a href="https://zalo.me/daihocquynhon?fbclid=IwY2xjawOpDgNleHRuA2FlbQIxMABicmlkETFVTHNvczNsMkdtSEExaU9Nc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHgT9mQ0ILxL1chNrQOQPuYfJ_3oRoqxDh0L0N1r-4pqs-EYwP-tq_n-7hjZW_aem_4PvCj3XG-fN0dpHh-KDhpg" class="social-icon" aria-label="Messenger">
                     <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M12 2C6.477 2 2 6.145 2 11.243c0 2.912 1.446 5.51 3.706 7.206V22l3.405-1.87c.908.252 1.87.387 2.889.387 5.523 0 10-4.145 10-9.243C22 6.145 17.523 2 12 2zm.993 12.535l-2.558-2.73-4.993 2.73 5.491-5.828 2.623 2.73 4.927-2.73-5.49 5.828z"/>
                     </svg>
                 </a>
-                <a href="#" class="social-icon" aria-label="YouTube">
+                <a href="https://www.youtube.com/@kenhtruongaihocquynhonyout1111/videos" class="social-icon" aria-label="YouTube">
                     <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
                     </svg>
                 </a>
-                <a href="#" class="social-icon" aria-label="GitHub">
+                <a href="https://github.com/GMIOS25/QNU-Event" class="social-icon" aria-label="GitHub">
                     <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.17 6.839 9.49.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.463-1.11-1.463-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.831.092-.646.35-1.086.636-1.336-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0 1 12 6.836c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.025 2.747-1.025.546 1.377.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.167 22 16.418 22 12c0-5.523-4.477-10-10-10z"/>
                     </svg>
